cde13c32b26ce3d8d57d6a96667324ba75d1df9a
Alexander V. Diemand <codieplusplus@apax.net>
2016-05-10 23:11:19 +0200

    bashed

--- src/Cpp/mk_DataExample.sh
+++ src/Cpp/mk_DataExample.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-for HPP in `sh ../../utils/find_hpp.sh ../../Code/Cpp/DataExample.md`; do
-  sh ../../utils/make_hpp.sh ${HPP}
-  sh ../../utils/make_cpp.sh ${HPP}
+for HPP in `bash ../../utils/find_hpp.sh ../../Code/Cpp/DataExample.md`; do
+  bash ../../utils/make_hpp.sh ${HPP}
+  bash ../../utils/make_cpp.sh ${HPP}
 done
 
 
 cd tests
 
-sh ../../../utils/make_test.sh  ../../../Code/Cpp/tests/DataExample.md 
+bash ../../../utils/make_test.sh  ../../../Code/Cpp/tests/DataExample.md 
 
 cd ..
